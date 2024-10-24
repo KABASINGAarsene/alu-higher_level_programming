4eab41562440bc29ad08297ae63bd031a63fcc2d
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-10-24 15:35:33 -0700

    Task 7

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]] or matrix == []:
+        print("")
+        return
+    else:
+        for liste in matrix:
+            for i in liste:
+                if i == liste[-1]:
+                    print("{:d}".format(i), end="\n")
+                else:
+                    print("{:d}".format(i), end=" ")
