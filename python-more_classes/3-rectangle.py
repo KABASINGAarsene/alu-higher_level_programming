2948a2387348c22196b22f7f78cb83d6b4258b40
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-05 14:09:09 -0800

    Task 4

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+This is a module documentation.
+"""
+
+
+class Rectangle:
+    """
+    This is a class documentation.
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return("")
+        return "\n".join("#" * self.width for _ in range(self.height))
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        area = self.__width * self.__height
+        return area
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            perimeter = 0
+        else:
+            perimeter = (self.__width + self.__height) * 2
+        return perimeter
