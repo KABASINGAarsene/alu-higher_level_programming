7cb1e3efaaa18afa0c864fb792c0ab2d3ffbacdd
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-05 01:33:11 -0800

    Task 4

---
+++ python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+"""
+This code create an empty class.
+"""
+
+
+class Square:
+    """
+    This is an empty class
+    """
+    def __init__(self, size=0):
+        try:
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        except Exception as e:
+            raise e
+
+    def area(self):
+        return self.__size**2
