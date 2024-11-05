874309a94dc23063abde29a1ad73aa9405dc4c79
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-05 01:27:03 -0800

    Task 2

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
 
+"""
+This code create an empty class.
+"""
+
 
 class Square:
+    """
+    This is an empty class
+    """
     def __init__(self, size):
         self.__size = size
