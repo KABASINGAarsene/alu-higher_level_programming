af9a1708b5d9402a64fe91229567775eae111e82
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-06 05:50:16 -0800

    Task 6

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+uhvbeijdskvniusahfwe ewijfewuhoiew
+wefwefhiewojfwpefwe
+wefwoeijfiweojfewf weofijwef
+"""
+
+
+class BaseGeometry:
+    """
+    This is an empty class
+    """
+    pass
