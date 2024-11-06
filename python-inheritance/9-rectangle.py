231a483ffd5f24cd1717cb8bc751c60e19bfcf98
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-06 12:10:49 -0800

    Task 10

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+QASDCFRTYUJMKBVCWQAPLuhvbeijdskvniusahfweewijfewuhoiew
+MKIUYTREWSZXAWERTFVBNMKwefwefhiewojfwpefwe
+QWERTYUIOLKJHGFDSXCVBNMwefwoeijfiweojfewf weofijwef
+"""
+
+
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")i
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """
+    MNBVEDCJfihvy78nwq9SOFIV3HYFCVNMKOLIUYTREWQAZ4V29Qgfdhbfdsgfshfsdfsds
+    """
+    def __init__(self, width, height):
+        # Validate width and height using integer_validator
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        # Initialize width and height as private attributes
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        # Returns the area of the rectangle
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
