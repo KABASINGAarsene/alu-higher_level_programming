ee8419a49e106a5a70aab801fdc847187063d2f4
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-06 05:36:10 -0800

    Task 3

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+ncxbcvdfskfkejdf
+dfgcsjdmcz,j ,DHVJ,ZDJV,Z
+RGERUGYTUSRGFS
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    kjdjdjfdjhfjxdjhfdkkdfjkf
+    jdyfsfskfhksfjhsk
+    mskyrfksjfdjfd
+    """
+    return type(obj) == a_class
