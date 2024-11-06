68cc20d317faaa1a76ef7647bb2c1641f25c1d88
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-06 05:41:13 -0800

    Task 4

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pytho
+"""
+wsxmbuerfvdjddbjfhffjffff
+ikmujnyhtgvfvqazQSDCEF
+SDGREHYTRNHDNhdgmjy,huytjhn jthgreht
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    fdsb sfbefwberg
+    dowe foiwk [qwdojwefwe
+    poiuytfcbgvfrffhksfcvwtq
+    """
+    return isinstance(obj, a_class)
