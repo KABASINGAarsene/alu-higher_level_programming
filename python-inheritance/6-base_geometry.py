4bbc76fe718c25ad539328fce15931ef0e8c56f4
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-06 05:53:51 -0800

    Task 7

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+uhvbeijpefwe
+jfiweojfewf weofijwef
+poiuytrewazxcvbnmmmmkjhgfdsmijkmujntgvrf
+"""
+
+
+class BaseGeometry:
+    """
+    This is an empty class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
