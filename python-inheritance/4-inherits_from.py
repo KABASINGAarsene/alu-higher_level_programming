ce866072755e7fb9eecb1acfaeed832d43150322
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-06 05:45:19 -0800

    TASK 5

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+fhkfvnrusifhuewairhgahfglejslFJvNSLNDLVL
+rfslhdvnwjherfhalerisaphjzsdvSdfvxcv
+cvbzests5eujrzdaegtjdnxdrhhtjhyjdnx
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    edcrfvtgyhnujmik
+    poiuygfdxxxxcvmnbvcfds
+    azxcdewsdfghjkoiuj,uygfddfdfddddddtyuiolkq
+    """
+    return isinstance(obj, a_class) and type(obj) != a_class
