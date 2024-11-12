ed51c3629e1c6877885ee6898e11774850511e28
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-12 12:57:25 -0800

    Task 1

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+WERTYUIOHGFDSXAQWERTYUI9OKMNBVFRFGHJU
+TRESXCFTRESDFGYUI
+JHGFDRFTYUIJHG
+"""
+
+
+def read_file(filename=""):
+    """
+    kjhgfcvbnmkoiuytrewasxcfrvfdeertyu
+    vfcdswertgertyuiolmnvc
+    oiuytrfedwasdfcMKIUYTREWSD
+    """
+    with open(filename, "r") as file:
+        for line in file:
+            print(line, end="")
