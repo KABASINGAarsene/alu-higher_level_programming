c06ff4117aaa4fdaa8b0a7d7f43e3f8170766a11
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-12 13:24:15 -0800

    Task 5

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+qwhduibyftvew fiwqugFBUYHASF
+QWsfcqbyuasbvuiweqnfv qwIDJOQWFAS
+wufgqbwjifknqwf qwf
+qfqwfnjqnwfqwfq
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    jihgfueiwqghfeiwog wesahviowasv
+    asvBASUVIJNWiaosjvwasv AC
+    ASVCQasfcjkqSANVC QSAFC
+    QSafcqadq
+    """
+    my_str_obj = json.loads(my_str)
+    return my_str_obj
