cabd393b353a9a8f949f7a922289118119f4e9a3
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-12 13:26:36 -0800

    Task 5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+ffjdkfjghfjdkdfjghfjdkfjghfjdkfjghfj
+kdjfhdjkdfhgfhjdskdfjghfhdjskdfjhj
+oirutyreuiwieuryteywuie
+"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    oiujnccbvbnxncbvcnxmcnbvcncbvnxcbvcnxcnbvcnxcb
+    mcnbvcnxcbvcbnxcbxncb
+    oeirutyrueirutyruieoirutyruieo
+    """
+    my_obj_json = json.dumps(my_obj)
+    with open(filename, "w") as file:
+        file.write(my_obj_json)
