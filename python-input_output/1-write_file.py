390e233b98520d417cdcfce952fb8a6b733964bf
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-12 13:05:07 -0800

    Task 2

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+ahf uianfhnjFGBLjKIUYTRERTYUJHDSWSXCV
+KIUYTREWSXDEDXCFRTFVGKIJKMNJUJUJ
+KJHGTRFFCXZXCVBNM
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    JHGFDSAWERTYUI
+    KJYTREWSXCVBN
+    KIUYTREWERTYUIOIUYTRED
+    """
+    with open(filename, "w") as file:
+        file.write(text)
+    return len(text)
