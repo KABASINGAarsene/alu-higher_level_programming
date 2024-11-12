560a9f6ba08cd56e111d5daf9c419b26214497ce
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-12 14:43:35 -0800

    Task 10

---
+++ python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+ashfgds asdghadsjhadsgd
+dsg adsghadsjgh asdgjhasgdsxg
+asgasgasgha AGUIagAGSKasgjdsgsg
+"""
+
+
+class Student:
+    """
+    Defines a student with first_name, last_name, and age.
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
