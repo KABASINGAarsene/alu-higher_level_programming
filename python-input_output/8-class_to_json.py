ed3d2739a24879ffd54b6807ec31eed51872cb36
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-12 14:36:45 -0800

    Task 9

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,5 +4,7 @@ Module to convert an object to a dictionary for JSON serialization.
 """
 
 def class_to_json(obj):
-    """Returns the dictionary description for JSON serialization of an object."""
+    """
+    Returns the dictionary description for JSON serialization of an object.
+    """
     return obj.__dict__
