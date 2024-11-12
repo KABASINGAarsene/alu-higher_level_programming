8ae60f5ca8bcbdd3a35ed60ff99b374c503e244a
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-12 13:21:20 -0800

    Task 4

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+frcdwqwertyuikikmnbvcdeszaqazswdftyuiookmjuyg
+tfredwsdfguiopombvcxswsdf
+frdfyuioiuytredcfgt
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    jertgfvcdsxcvbnmiopololololoiuytredesdxs
+    jujnhhnygvfrfdcdswszqasdf
+    mjkpoiuytrewwertjhgfdsasxcvbnm
+    """
+    my_obj_json = json.dumps(my_obj)
+    return my_obj_json
