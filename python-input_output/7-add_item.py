86fa570b9a9cd87aa0572d84fadc84ef5af38029
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-12 13:55:24 -0800

    Task 8

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -15,8 +15,12 @@ from 6-load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 try:
+    # Try to load existing items from the file
     items = load_from_json_file(filename)
 except FileNotFoundError:
+    # If the file doesn't exist, start with an empty list
     items = []
+# Add new arguments to the list
 items.extend(sys.argv[1:])
+# Save the updated list back to the file
 save_to_json_file(items, filename)
