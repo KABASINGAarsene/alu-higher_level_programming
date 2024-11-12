f9d271a9f475f9a98820e3a202e4c469821da8d3
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-12 13:48:25 -0800

    Task 7

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+rtyuidoiuytryeuierytyeuiruytyu
+rjtyryfhdjryu
+jrfhgthjdkfhhdjkfhgh
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    wertygfdfghjk
+    mkiuytgfrdrtyuiujh
+    gfdfghujhg
+    """
+    with open(filename, "r") as file:
+        json_object = file.read()
+        str_object = json.loads(json_object)
+        return str_object
