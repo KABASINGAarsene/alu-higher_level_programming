cf6a338eed67b7042b6b7b71ae7d790850c49de7
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-12 13:18:10 -0800

    Task 3

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+UYTREWWSERTYUIOKMKIUJNHYHYTFGVFDESDXSWQAZ
+MKLOPOIUYTREDCDSDFGHJ
+VBNMKPOGFDSXSDe
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    hushaQQQQQQQWERTYTREWERTYTREWERTYTRE
+    QWERTYUYTRTYUIMJUJNUYTGVFRFCDSASDFGFS
+    MNBVCXCVBNM,MNBV
+    """
+    with open(filename, "a") as file:
+        file.write(text)
+    return len(text)
