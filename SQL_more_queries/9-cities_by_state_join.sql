9dd1fcd49345deb4c5395e091f4c57a33d31d11f
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-14 15:42:14 -0800

    Task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
 -- listing cities contained in the database
-SELECT DISTINCT cities.id, cities.name, states.name
+SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states
-ON cities.id = cities.state_id
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
