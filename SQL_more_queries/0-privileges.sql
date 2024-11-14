02d9262a4e7a31b9ac0d523a49d32f404f6ce500
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-14 13:28:40 -0800

    Task 1

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,14 +1,3 @@
--- listing privileges of mysql users
--- comment
-/*
-#!/bin/bash
-USERS = ('user_0d_1' 'user_0d_2')
-HOST = 'localhost'
-
-for USER in '${USERS[@]}'
-do
-	SHOW GRANTS FOR '$USER'@'$HOST';
-done
-*/
+-- comments
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
