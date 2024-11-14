0c7be4d64bc256e1b752f1afb412355f8a2d5b2b
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-14 15:37:48 -0800

    Task 9

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,6 @@
 -- Use the database passed as an argument
-USE hbtn_0d_usa;
-
--- List cities of California, assuming we know the state_id for California
--- We first select the state_id of California and then use it to find the cities
-SELECT id, name
+SELECT cities.id, cities.name
 FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY id ASC;
+INNER JOIN states
+ON states.id = cities.state_id
+WHERE state_id = 1
