f07cd0ed51ce30a44b6b216a2e0b1bdfc3f7c240
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-14 15:54:29 -0800

    Task 14

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
--- commenst
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_shows.id  = tv_show_genres.show_id
-WHERE tv_show_genres.show_id IS NULL
-ORDER BY  tv_shows.title, tv_show_genres.genre_id ASC;
+-- comments
+SELECT genres.name AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
+FROM genres
+JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+GROUP BY genres.name
+HAVING COUNT(tv_show_genres.tv_show_id) > 0
+ORDER BY number_of_shows DESC;
