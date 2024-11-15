ca857d9a10a21385ebb88bcb745b4c7f631c5063
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-15 04:28:10 -0800

    Task 6

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,10 +4,10 @@ qwhduibyftvew fiwqugFBUYHASF
 QWsfcqbyuasbvuiweqnfv qwIDJOQWFAS
 wufgqbwjifknqwf qwf
 qfqwfnjqnwfqwfq
-
 """
 
 import json
+
 
 def save_to_json_file(my_obj, filename):
     """
@@ -15,12 +15,8 @@ def save_to_json_file(my_obj, filename):
     asvBASUVIJNWiaosjvwasv AC
     ASVCQasfcjkqSANVC QSAFC
     QSafcqadq
-
     """
-    # Convert sets to lists for JSON serialization
-    if isinstance(my_obj, set):
-        my_obj = list(my_obj)
-
-    # Attempt to write to the file
+    my_obj_json = json.dumps(my_obj)
     with open(filename, "w") as file:
-        json.dump(my_obj, file)
+        file.write(my_obj_json)
+
