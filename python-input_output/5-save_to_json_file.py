d8961a09633c3d1612dc6f52c119acdc7070b957
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-15 04:38:28 -0800

    Task 6

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,34 +1,21 @@
 #!/usr/bin/python3
+"""
+qwhduibyftvew fiwqugFBUYHASF
+QWsfcqbyuasbvuiweqnfv qwIDJOQWFAS
+wufgqbwjifknqwf qwf
+qfqwfnjqnwfqwfq
+"""
+
 import json
-import os
+
 
 def save_to_json_file(my_obj, filename):
     """
-    Serializes an object to a JSON file.
-
-    Parameters:
-    my_obj (any): The object to serialize. If it's a set, it will be converted to a list.
-    filename (str): The name of the file to write the JSON data to.
-
-    Raises:
-    PermissionError: If the file cannot be written due to permission issues.
+    jihgfueiwqghfeiwog wesahviowasv
+    asvBASUVIJNWiaosjvwasv AC
+    ASVCQasfcjkqSANVC QSAFC
+    QSafcqadq
     """
-    # Convert sets to lists for JSON serialization
-    if isinstance(my_obj, set):
-        my_obj = list(my_obj)
-    
-    try:
-        # Attempt to write to the file
-        with open(filename, "w") as file:
-            json.dump(my_obj, file)
-    except PermissionError as e:
-        print(f"PermissionError: {e}")
-
-# Example usage
-data = {1, 2, 3}
-filename = 'no_perm/file_7'
-
-# Ensure the directory exists and has the correct permissions
-os.makedirs(os.path.dirname(filename), exist_ok=True)
-
-save_to_json_file(data, filename)
+    my_obj_json = json.dumps(my_obj)
+    with open(filename, "w") as file:
+        file.write(my_obj_json)
