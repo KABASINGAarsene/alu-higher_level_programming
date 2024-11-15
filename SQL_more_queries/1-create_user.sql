571bf2025ea754b45e797de2ed9be4d8d9bf4d3b
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-15 04:13:39 -0800

    Task 2

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,8 @@
--- Create user if not exists with password
+-- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to the user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
--- Flush privileges to apply changes
+-- Flush privileges to ensure that all changes are applied
 FLUSH PRIVILEGES;
