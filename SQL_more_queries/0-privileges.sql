58a150c516151720705b56129a05299854f3894c
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-15 04:11:44 -0800

    Task 1

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,12 +1,5 @@
 -- List all privileges of the users
--- First create user_0d_1 with root privileges and user_0d_2 with specific privileges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Grant privileges
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
-
-SHOW GRANTS FOR user_0d_1@'localhost';
-SHOW GRANTS FOR user_0d_2@'localhost';
-
+-- Show privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Show privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
