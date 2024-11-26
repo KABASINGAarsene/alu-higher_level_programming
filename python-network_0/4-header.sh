d2d924af76cb092b0ecfe3a44ab5d52c55e77fbc
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-26 13:10:20 -0800

    Task 5

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-# Script to send a GET request with a custom header and display the response body
-
-# Check if URL argument is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Send GET request with the custom header and display the response body
 curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
