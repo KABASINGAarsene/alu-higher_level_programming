0776c7d1ec2e8c5c3397bf67d3e0b87debc705ce
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-27 03:14:16 -0800

    Task 1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""
-This script fetches the status of a given URL using urllib.
-"""
 import urllib.request
 
-if __name__ == "__main__":
-    # Change URL as needed during testing
-    url = "http://0.0.0.0:5050/status"
+url = "https://alu-intranet.hbtn.io/status"
 
-    # Fetch the URL and display the response
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+# Fetch the URL using urllib
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+# Display the response details
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body.decode('utf-8')))
 
