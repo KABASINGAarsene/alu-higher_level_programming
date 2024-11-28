0aaee5dfe40bdec0cce0bc269595a2f26e221bc6
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-28 05:48:25 -0800

    Task 5

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Takes in a URL as an arg, sends a GET request to the URL, and displays the body of the response
+curl -X GET -s -H "X-Holbertonschool-User-Id: 98" $1
