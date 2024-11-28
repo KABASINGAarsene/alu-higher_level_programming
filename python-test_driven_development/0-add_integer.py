45a4b2138dc7ad393b4736f4fe12efcab3178176
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-28 06:50:16 -0800

    Task 1

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 """
-Add_integer: adds two ints and returns their result
-Inputs a and b must be integers
-Floats are converted to ints before addition occurs
+Add_integer: adds two ints and returns their result.
+Inputs a and b must be integers.
+Floats are converted to ints before addition occurs.
 """
 
-
-def add_integer(a, b):
+def add_integer(a, b=98):
     """
-    Adds two ints and returns their result. Error-handled.
+    Adds two integers or floats (converted to integers) and returns their sum.
+    If a or b is not an integer or float, raises TypeError with a specific message.
     """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return (a + b)
+    
+    a = int(a)  # Cast to integer if it's a float
+    b = int(b)  # Cast to integer if it's a float
+    
+    return a + b
