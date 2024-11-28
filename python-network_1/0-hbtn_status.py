c9ccc4852e6b35225f01ad6bcc138155c1a64538
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-28 01:58:36 -0800

    Task 1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
-"""
-This script fetches the status of the URL "https://alu-intranet.hbtn.io/status"
-using urllib and displays the response body in the required format.
-"""
-import urllib.request
+""" fetches https://alu-intranet.hbtn.io/status """
+from urllib import request
 
-# URL to fetch
-url = "https://alu-intranet.hbtn.io/status"
+with request.urlopen("https://alu-intranet.hbtn.io/status") as fetched:
+    response = fetched.read()
 
-# Fetch the URL using urllib
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-
-# Display the response details
-print("Body response:")
-print("\t- type: {}".format(type(body)))
-print("\t- content: {}".format(body))
-print("\t- utf8 content: {}".format(body.decode('utf-8')))
+    print("Body response:")
+    print(f"\t- type: {type(response)}")
+    print(f"\t- content: {response}")
+    print(f"\t- utf8 content: {response.decode('utf8')}")
