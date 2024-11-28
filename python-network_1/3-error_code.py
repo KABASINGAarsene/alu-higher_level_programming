d3b2e8ea86444c0c34ce242f4d9b29a216b2e448
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-28 03:40:16 -0800

    Task 4

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,16 +1,37 @@
 #!/usr/bin/python3
 """
-cnsuivjweo washcwiuasv wdasvweav
-wsafwnisafuhSAFJNASF EASFASF
-ASFASFNWESAF ASDFIJNSAd fwasg
+This script sends an HTTP GET request to a provided URL, then displays the 
+body of the response (decoded in UTF-8). If an HTTP error occurs, it will 
+print the HTTP error code.
+
+Usage:
+    ./3-error_code.py <URL>
+
+Modules:
+    urllib.request - For sending HTTP requests.
+    urllib.error - For managing HTTP errors.
+    sys - For handling command-line arguments.
+
+Exceptions:
+    Handles urllib.error.HTTPError and prints the error code.
+
+Requirements:
+    - Must handle HTTPError exceptions.
+    - Use only urllib and sys.
+    - Must use the 'with' statement for making the request.
 """
-
-
 import urllib.request
 import urllib.error
 import sys
 
 def fetch_url(url):
+    """
+    This function takes a URL, sends a GET request, and displays the response body.
+    If an HTTPError occurs, it will print the error code.
+
+    Args:
+        url (str): The URL to send the GET request to.
+    """
     try:
         # Open the URL and read the response
         with urllib.request.urlopen(url) as response:
