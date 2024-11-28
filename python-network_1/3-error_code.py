f6166b36681c78373be1a1eb0136a256d8077ebe
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-28 03:34:45 -0800

    Task 4

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,37 +1,18 @@
 #!/usr/bin/python3
 """
-This script takes in a URL, sends a request to the URL, and displays the body
-of the response (decoded in utf-8). If an HTTPError exception occurs, it prints
-the error code.
-
-Usage:
-    ./3-error_code.py <URL>
-
-Modules:
-    urllib.request - For sending HTTP requests.
-    urllib.error - For managing HTTP errors.
-    sys - For handling command-line arguments.
-
-Requirements:
-    - Handle urllib.error.HTTPError exceptions.
-    - Display the HTTP error code when an exception is raised.
-    - Use only urllib and sys.
-    - Must use the with statement.
+This is module documentation. feafywefiuwe fwehfw
+wewuewhr weghquwheqw rbhqwiuhqwheqw afhiwqheqwe
 """
-import urllib.request
-import urllib.error
-import sys
+
+
+import requests
+
 
 if __name__ == "__main__":
-    # Get URL from the command-line argument
-    url = sys.argv[1]
+    """
+    Send a GET request to the specified URL
+    """
+    response = requests.get("https://alu-intranet.hbtn.io/status")
 
-    try:
-        # Send the GET request
-        with urllib.request.urlopen(url) as response:
-            # Read the response and decode it
-            body = response.read().decode("utf-8")
-            print(body)
-    except urllib.error.HTTPError as e:
-        # Handle HTTP errors by printing the error code
-        print(f"Error code: {e.code}")
+    # Display the body of the response with tabulation
+    print(f"Body response:\n\t{response.text}")
