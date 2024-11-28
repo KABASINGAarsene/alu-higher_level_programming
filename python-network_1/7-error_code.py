5c4e133ec01d9829f54732e994283961c17772de
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-28 05:23:26 -0800

    Task 7

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -8,7 +8,8 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    param = {'email': email}
-    r = requests.post(url, data=param)
-    print(r.text)
+    r = requests.get(url)
+    if r.status_code and int(r.status_code) >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
