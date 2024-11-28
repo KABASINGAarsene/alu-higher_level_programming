a044a0d1f873f9f3b33387c9f1e613b557631e05
KABASINGAarsene <a.kabasinga@alustudent.com>
2024-11-28 05:39:11 -0800

    Task 1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ Simple Python script that fetches https://intranet.hbtn.io/status
 import urllib.request
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
+    url = 'https://alu-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         content = response.read()
         content_type = type(content)
