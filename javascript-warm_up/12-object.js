f4d5526aa005dab0493694b3ef7946ab5bb05364
KABASINGAarsene <a.kabasinga@alustudent.com>
2025-01-28 03:41:04 -0800

    Task 12

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+// YOUR CODE HERE
+myObject.value = 89;
+// END OF YOUR CODE
+console.log(myObject);
