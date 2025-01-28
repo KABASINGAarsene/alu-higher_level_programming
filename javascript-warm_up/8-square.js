f490b2b90aee2bd1115782dce39849556e0c220e
KABASINGAarsene <a.kabasinga@alustudent.com>
2025-01-28 03:18:50 -0800

    Task 9

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,1 +1,11 @@
-gg
+#!/usr/bin/node
+
+const size = Number(process.argv[2]);
+
+if (isNaN(size) || !Number.isInteger(size)) {
+  console.log('Missing size');
+} else if (size > 0) {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
