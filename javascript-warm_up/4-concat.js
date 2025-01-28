b645aaeef1da16c924c06772dfb4e7ac7c55d449
KABASINGAarsene <a.kabasinga@alustudent.com>
2025-01-28 02:49:13 -0800

    Task 5

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 
+// Get arguments, defaulting to empty strings if missing
 const arg1 = process.argv[2] || '';
 const arg2 = process.argv[3] || '';
+
 console.log(`${arg1} is ${arg2}`);
