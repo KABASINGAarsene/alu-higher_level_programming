0e22415f7cedccd524b88c2ea2aeb59bdeb7b33b
KABASINGAarsene <a.kabasinga@alustudent.com>
2025-02-06 02:59:21 -0800

    Task

---
+++ javascript-web_jquery/4-script.js
@@ -0,0 +1,4 @@
+// jQuery to toggle between red and green classes on the <header> element when #toggle_header is clicked
+$('#toggle_header').click(function() {
+  $('header').toggleClass('red green');
+});
